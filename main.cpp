cffbf1957b0d8659a0bc8fe85e2a20556bb225f6
Tatev Avoyan <Tatev.avoyan@yahoo.com>
2021-08-24 15:47:10 +0400

    version 2

--- main.cpp
+++ main.cpp
@@ -1,21 +1,58 @@
+//#include "iostream"
+//
+//int main() {
+//
+//    int i = 0;
+//
+//    while(i < 2){
+//        printf("    *  \n");
+//        i = i + 1;
+//        while (i < 3){
+//            printf("  * ");
+//            i = i + 1;
+//        }
+//        printf("\n");
+//        while (i < 6){
+//            printf(" * ");
+//            i = i + 1;
+//        }
+//    }
+//
+//}
+
+
 #include "iostream"
 
 int main() {
 
     int i = 0;
+    int f = 2;
 
-    while(i < 2){
-        printf("    *  \n");
-        i = i + 1;
-        while (i < 3){
-            printf("  * ");
-            i = i + 1;
+    while(i < 3){
+        int b = 0;
+
+        while(b < 3 ){
+            if(b < f){
+                printf(" ");
+            } else{
+                printf("* ");
+            }
+
+            b++;
+
         }
+        f--;
         printf("\n");
-        while (i < 6){
-            printf(" * ");
-            i = i + 1;
-        }
+        i++;
     }
 
 }
+
+
+
+
+
+
+
+
+
